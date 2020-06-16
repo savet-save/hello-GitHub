f7127217813dd4a1bf1f3c708b1d434b3a33b59f
savet-save <savet_save@163.com>
2020-06-16 20:54:56 +0800

    delay_ms fix 2

--- test10.c
+++ test10.c
@@ -17,11 +17,11 @@ int main(int argc, char const *argv[])
 
 int delay_ms(int de_time)
 {
-    clock_t c_star = clock() * (CLOCKS_PER_SEC / 1000);
+    clock_t c_star = clock() / (CLOCKS_PER_SEC / 1000);
     if(de_time <= 0)
     {
         return -1;
     }
-    while((abs((clock() - c_star) * (CLOCKS_PER_SEC / 1000))) < de_time);
+    while((abs((clock() - c_star) / (CLOCKS_PER_SEC / 1000))) < de_time);
     return 0;
 }
