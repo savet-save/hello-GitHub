30126850c390781f1d592f47d09856830d48837c
savet-save <savet_save@163.com>
2020-07-06 14:34:22 +0800

    test10

--- test10.c
+++ test10.c
@@ -2,11 +2,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <signal.h>
 
 int main(int argc, char const *argv[])
 {
-
+    void *p = -1;
+    printf("%d", p);
     return 0;
 }
 
